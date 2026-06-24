33bfafded384f07883dc846a06986a91047b8b40
hoangphuckt7 <hoangphuckt7@gmail.com>
2026-06-25 00:49:10 +0700

    add image

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const PHOTOS = [
   "g5.webp",
   "g7.webp",
   "g9.webp",
+  "g10.jpg",
+  "g11.jpg",
 ];
 
 const card = document.querySelector(".card");
@@ -233,7 +235,7 @@ const wedding = {
        <p><span style="color:var(--muted-fg)">Khai tiệc</span>: <strong>11:30</strong></p>
       `,
     maps: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d683.3154409319504!2d108.00106475415461!3d14.345328904927433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x316bff9e19e85733%3A0xc09277f89e4774d5!2zMzAgQuG6oWNoIMSQ4bqxbmcsIEtvbiBUdW0sIFF14bqjbmcgTmfDo2ksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1782205198371!5m2!1svi!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>`,
-    location: "30 Bạch Đằng, P.Kon Tum, Tỉnh Quãng Ngãi",
+    location: "30 Bạch Đằng, P.Kon Tum, Tỉnh Quãng Ngãi (TP. Kon Tum cũ)",
     timeline1: "10:00",
     timeline2: "11:00",
     timeline3: "11:30",
