f99d7f5409cd84d509a8a145f94779bae03a628e
hoangphuckt7 <hoangphuckt7@gmail.com>
2026-06-25 15:41:56 +0700

    test

--- script.js
+++ script.js
@@ -329,7 +329,7 @@ function playMusic() {
   bgMusic.play()
     .then(() => {
       musicPlaying = true;
-      musicBtn.innerHTML = `<div class="playing">
+      musicBtn.innerHTML = `<div class="playing" style="index:999">
         <span class="playing__bar playing__bar1"></span>
         <span class="playing__bar playing__bar2"></span>
         <span class="playing__bar playing__bar3"></span>
@@ -351,7 +351,7 @@ function pauseMusic() {
   bgMusic.pause();
   musicPlaying = false;
 
-  musicBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" style="color: #ffffff; margin:10px">
+  musicBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" style="color: #ffffff; margin:10px; index:999">
         <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z" class="jsx-96d6f55b08df8af4"></path>
       </svg>`;
   musicBtn.classList.remove("playing");
